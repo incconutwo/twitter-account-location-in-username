751b78e5b3c6677e45e976c5f78c99622ef8de1f
incconutwo <89911479+incconutwo@users.noreply.github.com>
2025-12-24 20:14:28 +0100

    chore: Delete files

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setIcon({
-    path: {
-      "16": "icons/16.png",
-      "48": "icons/48.png",
-      "128": "icons/128.png"
-    }
-  });
-});
